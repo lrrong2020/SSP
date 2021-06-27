759ac7decc6f679dc94fddb5c2315a498fdfd170
lrrong2020 <73475782+lrrong2020@users.noreply.github.com>
2021-06-28 03:21:47 +0800

    Indentation and comments

--- childWebpage/JS_childWebPage/realScript.js
+++ childWebpage/JS_childWebPage/realScript.js
@@ -1,5 +1,5 @@
-const DOUBLE_SAMPLE_MIN = 15099
-const DOUBLE_SAMPLE_MAX = 20459
+const DOUBLE_SAMPLE_MIN = 35961
+const DOUBLE_SAMPLE_MAX = 36460
 const SINGLE_SAMPLE_MIN = 0
 const SINGLE_SAMPLE_MAX = 2710
 const TS_CONVERTER_CONST = 1000
@@ -14,29 +14,21 @@ var myChart
 var singlePlotArr = [[]]
 var doublePlotArr = [[]]
 
+
 console.log('loaded')
 
 
-
+//pop-up
 function openwindow(){
-		//获取弹窗得div
 		var modal = document.getElementById('myModal');
-		// 获取 <span> 元素，用于关闭弹窗 （X）
 		var span = document.getElementsByClassName("close")[0];
-		//获取弹窗中得确定按钮
 		var ok=document.getElementsByClassName("ok")[0];
-		//获取弹窗中得取消按钮
 		var no=document.getElementsByClassName("no")[0];
-		//窗体弹出
 		modal.style.display = "block";
-
-		// 在用户点击其他地方时，关闭弹窗
 		window.onclick = function(event) {
-			//点击窗口外内容，关闭窗口
 			if (event.target == modal) modal.style.display = "none";
 		}
 	}
-
 
 
 //check if 2d array has empty child and return empty index
@@ -68,11 +60,6 @@ function appendArr(num, arr){
 	}	
 }
 
-function presentArray(sb){
-	return sb
-
-}
-
 
 //fetch needed data in remote repository and store them in arrays
 let doubleCameraData = 'https://raw.githubusercontent.com/lrrong2020/Smart-Campus/main/double_camera.csv'
@@ -85,8 +72,7 @@ fetch (singleCameraData)
 
 fetch (doubleCameraData)
 .then(response => response.text())
-.then(result => doubleCameraArr = presentArray(result.split('\n').map(function (line){return line.split(',')})))
-
+.then(result => doubleCameraArr = result.split('\n').map(function (line){return line.split(',')}))
 
 
 //convert timestamp to Italy time
@@ -94,6 +80,8 @@ function timeConverter(arr, i){
 	var ts = parseInt(arr[i][0])
 	return (new Date(ts * TS_CONVERTER_CONST + TWO_HOURS)).toISOString()
 }
+
+
 //format time string
 function formatTime(time){
 	return (time.slice(11,13) + ":" + time.slice(14,16) + ":" + time.slice(17,19))
@@ -102,9 +90,6 @@ function formatTime(time){
 
 //plot
 function plot(arr){
-		
-
-		
 		var ctx = document.getElementById('myChart').getContext('2d')
 		var lb = []
 		var dt = []
@@ -120,51 +105,32 @@ function plot(arr){
 				label: '# of Students',
 				data: dt,
 				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
+					'rgba(52, 128, 235, 0.2)',
 				],
 				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
+					'rgba(52, 128, 235, 1)',
 				],
-				borderWidth: 1
+				borderWidth: 3
 			}]
 		},
 		options: {
 			scales: {
-				y: {
+				y: {	
+					suggestedMin: 0,
+					suggestedMax: 50,
 					beginAtZero: true
 				}
 			}
 		}
 	});
-setTimeout(function(){
-		myChart.destroy()
-		
-		
-		}, 3000)
+setTimeout(function(){myChart.destroy()}, 3000)
 		document.getElementById("loading").style.display = "none";
 		setTimeout(function(){document.getElementById("loading").style.display = "block";},3000)
-
 }
 
 
 //present single camera array
 var singleCounter = SINGLE_SAMPLE_MIN
-
-
-
-
-
-
 
 setInterval(function (){
 if(singleCounter <= SINGLE_SAMPLE_MAX){
@@ -174,14 +140,9 @@ if(singleCounter <= SINGLE_SAMPLE_MAX){
 	if(singlePlotArr[0].length === 0){singlePlotArr[0] = [formatTime(lastTime),studentNumberFromSingle]}
 	else {appendArr([formatTime(lastTime), studentNumberFromSingle], singlePlotArr)}
 
-	console.log("single: " + lastTime + "no: " + studentNumberFromSingle)
-	
-
 	plot(singlePlotArr)
 
 	document.getElementById('camera1').innerHTML=studentNumberFromSingle
-
-	//console.log(formatTime(lastTime))
 	singleCounter = singleCounter + 1
 }
 }, REAL_INTERVAL)
@@ -203,7 +164,5 @@ if(doubleCounter <= DOUBLE_SAMPLE_MAX){
 	document.getElementById('camera2').innerHTML=studentNumberFromDouble
 
 	plot(doublePlotArr)
-	//console.log(formatTime(lastTime))
 	doubleCounter = doubleCounter + 2}
 }, REAL_INTERVAL)
-
